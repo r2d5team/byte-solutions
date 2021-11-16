785c8c6d4f91bb7edb1b000630e586d3d4cc0db7
BiplopDey <52823579+BiplopDey@users.noreply.github.com>
2021-11-16 15:23:58 +0100

    add db

--- add.php
+++ add.php
@@ -1,4 +1,5 @@
 <?php
+require_once 'Database.php';
 //Validamos que hayan llegado estas variables, y que no esten vacias:
 if (isset($_POST["name"], $_POST["email"], $_POST["querytitle"], $_POST["message"]) and $_POST["name"] !="" and $_POST["email"]!="" and $_POST["querytitle"]!="" and $_POST["message"]!="" ){
     //traspasamos a variables locales, para evitar complicaciones con las comillas:
@@ -7,11 +8,9 @@ $formulario = [
     "email" => $_POST["email"],
     "querytitle" => $_POST["querytitle"],
     "message" => $_POST["message"],];
-
-    //add($formulario   )
-var_dump ($formulario);
-
-
+    
+    Database::create($formulario["name"],$formulario["email"],$formulario["querytitle"],$formulario["message"]);
+    
 } else {
 
 echo '<p>Por favor, complete el <a href="create.php">formulario</a></p>';
--- database.php
+++ database.php
@@ -1,10 +1,21 @@
 <?php
 
 class Database {
-    private $appointments = [];
+    private static $appointments = [];
+    private static $id=0;
     //private $users=[];
-    public function add($name,$email,$querytitle,$message){
+    public static function create($name, $email,$querytitle,$message){
+        self::$id++;
         $datetime = date("Y-m-d h:i:sa");
+        array_push(self::$appointments, [
+            'id' => self::$id,
+            'name' => $name,
+            'email' => $email,
+            'querytitle' => $querytitle,
+            'message' => $message,
+            'datetime' => $datetime
+            ]
+        );
     }
     public function getAll() {
         return $this->appointments;
