846d1b4951127f5921f289f444dc94fe65b900e9
thisisariann <ari.menendez13@gmail.com>
2021-11-17 23:11:27 +0100

    public folder

---
+++ public/landingpageView.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+    <link rel="stylesheet" href="css/generalstyles.css" />
+    <title>The Software Office</title>
+</head>
+
+<body>
+    <?php
+    require_once __DIR__ . '/../Database.php';
+echo "
+    <header class='header-landingpg'>
+        <h1 class='title'>The Software Office</h1>
+        <h2 class='eslogan'>Looking for solutions</h2>
+    </header>
+    <section class='description'>
+        <p class='description-p'>If you want to create an appointment<b> 'Click' on CREATE</b><br>
+            or <br>If you want to check your appointment list history Please,<b> 'Click' on CHECKLIST</b></p>
+    </section>
+    <section class='btn-section'>
+        <button class='btn-general'>
+            <p class='btn-p'>CREATE</p>
+        </button>
+        <p class='or'>OR</p>
+        <button class='btn-general'>
+            <p class='btn-p'>CHECK LIST</p>
+        </button>
+    </section>
+</body> "
+
+
+
+  ?>
+
+
+
+</html>
