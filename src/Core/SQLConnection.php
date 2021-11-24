8ef7dea95b0e650a305fda23cf6cd1c8ecf64b7b
BiplopDey <52823579+BiplopDey@users.noreply.github.com>
2021-11-24 12:24:29 +0100

    config

--- src/Core/SQLConnection.php
+++ src/Core/SQLConnection.php
@@ -11,10 +11,11 @@ class SQLConnection {
     
     public function __construct()
     {
-        $host = 'localhost';
-        $user = 'root';
-        $pass = '';
-        $database = 'consultas_cto';
+        $config = new Config;
+        $host = $config->host;
+        $user = $config->user;
+        $pass = $config->pass;
+        $database = $config->database;
         $charset = 'utf-8';
         $options = [PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_ASSOC];
 
